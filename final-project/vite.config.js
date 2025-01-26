7aff70e52b127a6a1271c6c360d0011a043bf262
udongmandu <61107496+udongmandu@users.noreply.github.com>
2025-01-26 15:34:50 +0900

    test

---
+++ final-project/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+    https: true,
+  },
+});
